fb96c91999c839748fd0bb4fae5e69f5d232e7ea
nnicosotelo <nicosote27@gmail.com>
2022-04-08 15:24:46 -0300

    primer comit js

---
+++ js/fundamentos/01-Variables.js
@@ -0,0 +1,84 @@
+// alert('holaa');
+
+// mensajes en la consola
+// console.log('este es un mensaje');
+// console.log('este es otro msj');
+
+// mostrar en el navegador
+// document.write('este es un nuevo mensaje');  
+
+// variables
+// las variables son espacios en memoria que se usa para guardar datos
+
+// declara: se las crea
+// inicializa: asignar un valor
+
+var color;
+color ="azul";
+
+let nombre = "rojo";
+let apellido = "nico";
+
+// son variables constantes, es decir que no se puede cambiar el valor
+
+const pi = 3.14;
+
+// tipos de datos
+
+// string
+let cliente= 'Maite'
+let alumno= 'Lionel Messi'
+let mensaje='Moriste en Madrid'
+
+// number
+let number1 = 9
+let number2 = 12
+let number3 = 18
+
+// booleans
+let comprar = true
+let vender = false
+
+// null
+let mochila = null
+
+// undefined 
+let helado;
+
+// array 
+let arreglo = ['nico',27,true]
+let clientes = ['Enzo', 'Nicolas','Perez']
+
+// objects
+let persona = {
+    nombre:'Emma',
+    apellido:'Sotelin',
+    edad:3
+}
+
+// operadores
+
+// aritmeticos
+let suma = number1 + number2;
+let resta = number1 - number3;
+let division = number1 / number2;
+let multiplicacion = number3 * number2;
+let resto = number1 % number3;
+
+// unitarios
+++number1 
+--number3
+
+// relacionales
+num1 = 18;
+num2 = '18';
+console.log(num1 === num2);
+console.log(num1 >= num2);
+console.log(number1 <= number2)
+console.log(number1 != number2)
+
+console.log("El resultado de la suma " , suma);
+
+// negacion
+let activado = true;
+console.log(!activado);
