25288e44e78551a7f507eab126a6002af3470789
nnicosotelo <nicosote27@gmail.com>
2022-05-26 15:54:48 -0300

    lista de tareas corregida

--- to do/js/app.js
+++ to do/js/app.js
@@ -1,36 +1,50 @@
-let tareas = [];
-let counter = document.getElementById("contador");
-let contadorTarjetas = document.getElementById("contenedor");
+let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+// El método JSON.parse() analiza una cadena de texto como JSON, transformando opcionalmente  el valor producido por el análisis. Retorna el objeto que se corresponde con el texto JSON entregado.
 
-const holdSubmit = function (e) {
+let counter = document.getElementById("contador");
+let contenedorTarjetas = document.getElementById("contenedor");
+
+const handleSubmit = function (e) {
   e.preventDefault();
   let tarea = document.getElementById("text_tarea");
   tareas.push(tarea.value);
-  console.log(tareas);
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+  // El método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON
+
   tarea.value = "";
   tarea.select();
-  counter.innerText = tareas.length;
   agregarTarjeta();
 };
 
 const agregarTarjeta = function () {
   contenedorTarjetas.innerHTML = "";
+
   tareas.forEach(function (tarea, index) {
     let tarjeta = document.createElement("div");
     tarjeta.classList = "card mb-1";
 
-    let contenidoTarjeta = ` <div
-        class="card-body d-flex justify-content-between align-items-center"
-      >
-        <div>
-          <span>Estudiar bootstrap</span>
-          <button class="btn btn-danger btn-sm">x</button>
-        </div>`;
+    let contenidoTarjeta = `<div
+    class="card-body d-flex justify-content-between align-items-center"
+  >
+    <span>📌 ${tarea}</span>
+    <button class="btn btn-danger btn-sm" onclick="borrarTarea(${index})">x</button>
+  </div>`;
 
     tarjeta.innerHTML = contenidoTarjeta;
 
     contenedorTarjetas.appendChild(tarjeta);
   });
+  counter.innerText = tareas.length;
 };
 
-document.getElementById("formulario").addEventListener("submit", holdSubmit);
+function borrarTarea(index) {
+  //   console.log(index);
+  tareas.splice(index, 1);
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+
+  agregarTarjeta();
+}
+
+document.getElementById("formulario").addEventListener("submit", handleSubmit);
+
+agregarTarjeta();
